173a3dc7114d9be9c82c39da3494559ed8f4f327
pujitha050 <158188612+pujitha050@users.noreply.github.com>
2024-02-01 22:40:48 +0530

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,16 @@
+def bubble_sort(arr):
+ n = len(arr)
+ for i in range(n):
+ 
+# Last i elements are already in place
+  for j in range(0, n - i - 1):
+ 
+# Swap if the element found is greater than the next element
+   if arr[j] > arr[j + 1]:
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+# Example usage
+numbers = [5, 2, 8, 12, 3]
+print("Before sorting:", numbers)
+bubble_sort(numbers)
+print("After sorting:", numbers)
